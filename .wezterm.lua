88eb6e9f3d84e6c5611ae446352060df8c391686
Alexis <corporal461@gmail.com>
2023-02-11 13:22:57 +0800

    format and change line_height

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,258 +2,258 @@ local wezterm = require("wezterm")
 local is_dark = true
 
 local function is_vi_process(pane)
-	return pane:get_foreground_process_name():find("n?vim") ~= nil
+  return pane:get_foreground_process_name():find("n?vim") ~= nil
 end
 
 local function conditional_activate_pane(window, pane, pane_direction, vim_direction)
-	if is_vi_process(pane) then
-		window:perform_action(wezterm.action.SendKey({ key = vim_direction, mods = "ALT" }), pane)
-	else
-		window:perform_action(wezterm.action.ActivatePaneDirection(pane_direction), pane)
-	end
+  if is_vi_process(pane) then
+    window:perform_action(wezterm.action.SendKey({ key = vim_direction, mods = "ALT" }), pane)
+  else
+    window:perform_action(wezterm.action.ActivatePaneDirection(pane_direction), pane)
+  end
 end
 
 wezterm.on("ActivatePaneDirection-right", function(window, pane)
-	conditional_activate_pane(window, pane, "Right", "l")
+  conditional_activate_pane(window, pane, "Right", "l")
 end)
 wezterm.on("ActivatePaneDirection-left", function(window, pane)
-	conditional_activate_pane(window, pane, "Left", "h")
+  conditional_activate_pane(window, pane, "Left", "h")
 end)
 wezterm.on("ActivatePaneDirection-up", function(window, pane)
-	conditional_activate_pane(window, pane, "Up", "k")
+  conditional_activate_pane(window, pane, "Up", "k")
 end)
 wezterm.on("ActivatePaneDirection-down", function(window, pane)
-	conditional_activate_pane(window, pane, "Down", "j")
+  conditional_activate_pane(window, pane, "Down", "j")
 end)
 
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function(cmd)
-	local _, _, window = mux.spawn_window(cmd or {})
-	window:gui_window():set_position(412, 45)
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():set_position(412, 45)
 end)
 
 wezterm.on("update-right-status", function(window)
-	window:set_right_status(wezterm.format({
-		{ Attribute = { Intensity = "Bold" } },
-		{ Text = wezterm.strftime(" %A, %d %B %Y %I:%M %p ") },
-	}))
+  window:set_right_status(wezterm.format({
+    { Attribute = { Intensity = "Bold" } },
+    { Text = wezterm.strftime(" %A, %d %B %Y %I:%M %p ") },
+  }))
 end)
 
 return {
-	font = wezterm.font_with_fallback({
-		"JetBrainsMono NF",
-		"Liga SFMono Nerd Font",
-		"Apple Color Emoji",
-	}),
+  font = wezterm.font_with_fallback({
+    "JetBrainsMono NF",
+    "Liga SFMono Nerd Font",
+    "Apple Color Emoji",
+  }),
+  -- default_prog = { "pwsh.exe" },
+  font_size = 11,
+  max_fps = 120,
+  enable_wayland = false,
+  pane_focus_follows_mouse = false,
+  warn_about_missing_glyphs = false,
+  show_update_window = false,
+  check_for_updates = false,
+  line_height = 1.0,
+  -- window_decorations = "RESIZE",
+  window_close_confirmation = "NeverPrompt",
+  audible_bell = "Disabled",
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  initial_cols = 105,
+  initial_rows = 32,
+  inactive_pane_hsb = {
+    saturation = 1.0,
+    brightness = is_dark and 0.85 or 0.95,
+  },
+  enable_scroll_bar = false,
+  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = false,
+  show_new_tab_button_in_tab_bar = false,
+  window_background_opacity = 1.0,
+  tab_max_width = 10,
+  hide_tab_bar_if_only_one_tab = true,
+  disable_default_key_bindings = false,
+  front_end = "WebGpu",
+  colors = {
+    -- The default text color
+    foreground = "#BBC2CF",
+    -- The default background color
+    background = "#0E131B",
 
-	font_size = 11,
-	max_fps = 120,
-	enable_wayland = false,
-	pane_focus_follows_mouse = false,
-	warn_about_missing_glyphs = false,
-	show_update_window = false,
-	check_for_updates = false,
-	line_height = 1.2,
-	-- window_decorations = "RESIZE",
-	window_close_confirmation = "NeverPrompt",
-	audible_bell = "Disabled",
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
-	initial_cols = 105,
-	initial_rows = 28,
-	inactive_pane_hsb = {
-		saturation = 1.0,
-		brightness = is_dark and 0.85 or 0.95,
-	},
-	enable_scroll_bar = false,
-	tab_bar_at_bottom = true,
-	use_fancy_tab_bar = false,
-	show_new_tab_button_in_tab_bar = false,
-	window_background_opacity = 1.0,
-	tab_max_width = 50,
-	hide_tab_bar_if_only_one_tab = true,
-	disable_default_key_bindings = false,
-	front_end = "WebGpu",
-	colors = {
-		-- The default text color
-		foreground = "#BBC2CF",
-		-- The default background color
-		background = "#0E131B",
+    -- Overrides the cell background color when the current cell is occupied by the
+    -- cursor and the cursor style is set to Block
+    cursor_fg = "#000000",
+    -- Overrides the text color when the current cell is occupied by the cursor
+    cursor_bg = "#A9B1D6",
+    -- Specifies the border color of the cursor when the cursor style is set to Block,
+    -- or the color of the vertical or horizontal bar when the cursor style is set to
+    -- Bar or Underline.
+    cursor_border = "#BBC2CF",
 
-		-- Overrides the cell background color when the current cell is occupied by the
-		-- cursor and the cursor style is set to Block
-		cursor_fg = "#000000",
-		-- Overrides the text color when the current cell is occupied by the cursor
-		cursor_bg = "#A9B1D6",
-		-- Specifies the border color of the cursor when the cursor style is set to Block,
-		-- or the color of the vertical or horizontal bar when the cursor style is set to
-		-- Bar or Underline.
-		cursor_border = "#BBC2CF",
+    -- the foreground color of selected text
+    selection_fg = "#BBC2CF",
+    -- the background color of selected text
+    selection_bg = "#292E42",
 
-		-- the foreground color of selected text
-		selection_fg = "#BBC2CF",
-		-- the background color of selected text
-		selection_bg = "#292E42",
+    -- The color of the scrollbar "thumb"; the portion that represents the current viewport
+    scrollbar_thumb = "#222222",
 
-		-- The color of the scrollbar "thumb"; the portion that represents the current viewport
-		scrollbar_thumb = "#222222",
+    -- The color of the split lines between panes
+    split = "#7AA2F7",
 
-		-- The color of the split lines between panes
-		split = "#7AA2F7",
+    ansi = {
+      "#000000",
+      "#ECBE7B",
+      "#98BE65",
+      "olive",
+      "#7AA2F7",
+      "#C678DD",
+      "#1ABC9C",
+      "#ABABAF",
+    },
+    brights = {
+      "#ABABAF",
+      "#DB4B4B",
+      "#87AF87",
+      "#ECBE7B",
+      "#7AA2F7",
+      "#BB9AF7",
+      "#6F87AF",
+      "#BBC2CF",
+    },
 
-		ansi = {
-			"#000000",
-			"#ECBE7B",
-			"#98BE65",
-			"olive",
-			"#7AA2F7",
-			"#C678DD",
-			"#1ABC9C",
-			"#ABABAF",
-		},
-		brights = {
-			"#ABABAF",
-			"#DB4B4B",
-			"#87AF87",
-			"#ECBE7B",
-			"#7AA2F7",
-			"#BB9AF7",
-			"#6F87AF",
-			"#BBC2CF",
-		},
+    -- Arbitrary colors of the palette in the range from 16 to 255
+    indexed = { [136] = "#D19A66" },
 
-		-- Arbitrary colors of the palette in the range from 16 to 255
-		indexed = { [136] = "#D19A66" },
+    -- Since: 20220319-142410-0fcdea07
+    -- When the IME, a dead key or a leader key are being processed and are effectively
+    -- holding input pending the result of input composition, change the cursor
+    -- to this color to give a visual cue about the compose state.
+    compose_cursor = "#ECBE7B",
 
-		-- Since: 20220319-142410-0fcdea07
-		-- When the IME, a dead key or a leader key are being processed and are effectively
-		-- holding input pending the result of input composition, change the cursor
-		-- to this color to give a visual cue about the compose state.
-		compose_cursor = "#ECBE7B",
+    -- Colors for copy_mode and quick_select
+    -- available since: 20220807-113146-c2fee766
+    -- In copy_mode, the color of the active text is:
+    -- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
+    -- 2. selection_* otherwise
+    copy_mode_active_highlight_bg = { Color = "#000000" },
+    -- use `AnsiColor` to specify one of the ansi color palette values
+    -- (index 0-15) using one of the names "Black", "Maroon", "Green",
+    --  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
+    -- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
+    copy_mode_active_highlight_fg = { AnsiColor = "Black" },
+    copy_mode_inactive_highlight_bg = { Color = "#52ad70" },
+    copy_mode_inactive_highlight_fg = { AnsiColor = "White" },
 
-		-- Colors for copy_mode and quick_select
-		-- available since: 20220807-113146-c2fee766
-		-- In copy_mode, the color of the active text is:
-		-- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
-		-- 2. selection_* otherwise
-		copy_mode_active_highlight_bg = { Color = "#000000" },
-		-- use `AnsiColor` to specify one of the ansi color palette values
-		-- (index 0-15) using one of the names "Black", "Maroon", "Green",
-		--  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
-		-- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
-		copy_mode_active_highlight_fg = { AnsiColor = "Black" },
-		copy_mode_inactive_highlight_bg = { Color = "#52ad70" },
-		copy_mode_inactive_highlight_fg = { AnsiColor = "White" },
+    quick_select_label_bg = { Color = "peru" },
+    quick_select_label_fg = { Color = "#ffffff" },
+    quick_select_match_bg = { AnsiColor = "Navy" },
+    quick_select_match_fg = { Color = "#ffffff" },
+  },
+  keys = {
+    {
+      mods = "ALT",
+      key = [[\]],
+      action = wezterm.action({
+        SplitHorizontal = { domain = "CurrentPaneDomain" },
+      }),
+    },
+    {
+      mods = "ALT|SHIFT",
+      key = [[|]],
+      action = wezterm.action.SplitPane({
+        top_level = true,
+        direction = "Right",
+        size = { Percent = 50 },
+      }),
+    },
+    {
+      mods = "ALT",
+      key = [[-]],
+      action = wezterm.action({
+        SplitVertical = { domain = "CurrentPaneDomain" },
+      }),
+    },
+    {
+      mods = "ALT|SHIFT",
+      key = [[_]],
+      action = wezterm.action.SplitPane({
+        top_level = true,
+        direction = "Down",
+        size = { Percent = 50 },
+      }),
+    },
+    {
+      key = "n",
+      mods = "ALT",
+      action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
+    },
+    {
+      key = "Q",
+      mods = "ALT",
+      action = wezterm.action({ CloseCurrentTab = { confirm = false } }),
+    },
+    { key = "q", mods = "ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+    { key = "z", mods = "ALT", action = wezterm.action.TogglePaneZoomState },
+    { key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
+    { key = "h", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+    { key = "j", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+    { key = "k", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+    { key = "l", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
 
-		quick_select_label_bg = { Color = "peru" },
-		quick_select_label_fg = { Color = "#ffffff" },
-		quick_select_match_bg = { AnsiColor = "Navy" },
-		quick_select_match_fg = { Color = "#ffffff" },
-	},
-	keys = {
-		{
-			mods = "ALT",
-			key = [[\]],
-			action = wezterm.action({
-				SplitHorizontal = { domain = "CurrentPaneDomain" },
-			}),
-		},
-		{
-			mods = "ALT|SHIFT",
-			key = [[|]],
-			action = wezterm.action.SplitPane({
-				top_level = true,
-				direction = "Right",
-				size = { Percent = 50 },
-			}),
-		},
-		{
-			mods = "ALT",
-			key = [[-]],
-			action = wezterm.action({
-				SplitVertical = { domain = "CurrentPaneDomain" },
-			}),
-		},
-		{
-			mods = "ALT|SHIFT",
-			key = [[_]],
-			action = wezterm.action.SplitPane({
-				top_level = true,
-				direction = "Down",
-				size = { Percent = 50 },
-			}),
-		},
-		{
-			key = "n",
-			mods = "ALT",
-			action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
-		},
-		{
-			key = "Q",
-			mods = "ALT",
-			action = wezterm.action({ CloseCurrentTab = { confirm = false } }),
-		},
-		{ key = "q", mods = "ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-		{ key = "z", mods = "ALT", action = wezterm.action.TogglePaneZoomState },
-		{ key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
-		{ key = "h", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "j", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "k", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "l", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+    { key = "h", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
+    { key = "j", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
+    { key = "k", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
+    { key = "l", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
 
-		{ key = "h", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
-		{ key = "j", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
-		{ key = "k", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
-		{ key = "l", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
-
-		{ key = "[", mods = "ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
-		{ key = "]", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
-		{ key = "{", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(-1) },
-		{ key = "}", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(1) },
-		{ key = "v", mods = "ALT", action = wezterm.action.ActivateCopyMode },
-		{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
-		{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-		{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-		{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
-		{ key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
-		{ key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
-		{ key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
-		{ key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
-		{ key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
-		{ key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
-		{ key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
-		{ key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
-		{ key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
-	},
-	hyperlink_rules = {
-		{
-			regex = "\\b\\w+://[\\w.-]+:[0-9]{2,15}\\S*\\b",
-			format = "$0",
-		},
-		{
-			regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
-			format = "$0",
-		},
-		{
-			regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-			format = "mailto:$0",
-		},
-		{
-			regex = [[\bfile://\S*\b]],
-			format = "$0",
-		},
-		{
-			regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-			format = "$0",
-		},
-		{
-			regex = [[\b[tT](\d+)\b]],
-			format = "https://example.com/tasks/?t=$1",
-		},
-	},
+    { key = "[", mods = "ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
+    { key = "]", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
+    { key = "{", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(-1) },
+    { key = "}", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(1) },
+    { key = "v", mods = "ALT", action = wezterm.action.ActivateCopyMode },
+    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
+    { key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+    { key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+    { key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+    { key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
+    { key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
+    { key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
+    { key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
+    { key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
+    { key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
+    { key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
+    { key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
+    { key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
+  },
+  hyperlink_rules = {
+    {
+      regex = "\\b\\w+://[\\w.-]+:[0-9]{2,15}\\S*\\b",
+      format = "$0",
+    },
+    {
+      regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
+      format = "$0",
+    },
+    {
+      regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+      format = "mailto:$0",
+    },
+    {
+      regex = [[\bfile://\S*\b]],
+      format = "$0",
+    },
+    {
+      regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+      format = "$0",
+    },
+    {
+      regex = [[\b[tT](\d+)\b]],
+      format = "https://example.com/tasks/?t=$1",
+    },
+  },
 }
