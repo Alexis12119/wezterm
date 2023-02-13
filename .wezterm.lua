480b371f6b7a2ec78d34372dca53982aee30e28b
Alexis <corporal461@gmail.com>
2023-02-13 17:16:46 +0800

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -46,6 +46,7 @@ return {
 		"Liga SFMono Nerd Font",
 		"Apple Color Emoji",
 	}),
+
 	-- default_prog = { "pwsh.exe" },
 	font_size = 11,
 	max_fps = 120,
@@ -93,7 +94,7 @@ return {
 		-- Specifies the border color of the cursor when the cursor style is set to Block,
 		-- or the color of the vertical or horizontal bar when the cursor style is set to
 		-- Bar or Underline.
-		cursor_border = "#121212",
+		cursor_border = "#181818",
 
 		-- the foreground color of selected text
 		selection_fg = "#BBC2CF",
@@ -104,7 +105,7 @@ return {
 		scrollbar_thumb = "#222222",
 
 		-- The color of the split lines between panes
-		split = "#7AA2F7",
+		split = "#6B7478",
 
 		ansi = {
 			"#000000",
